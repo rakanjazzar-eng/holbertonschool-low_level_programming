f169ee76655ac2ba8751585d938d986bef6655ba
Kony Jazzar <rakanjazzar@gmail.com>
2026-07-09 20:28:02 +0300

    add 9-print_combo.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible combinations of single-digit numbers,
+ * separated by a comma and a space, in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
