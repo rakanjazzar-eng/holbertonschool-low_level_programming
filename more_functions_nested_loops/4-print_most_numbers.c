26821cf6b831a1bd6337bf9307ceae754bccd005
Kony Jazzar <rakanjazzar@gmail.com>
2026-07-10 19:08:41 +0300

    Fix print_most_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
- *
- * Return: void.
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int num;
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		_putchar(num);
+		if (num != '2' && num != '4')
+			_putchar(num);
 	}
 	_putchar('\n');
 }
