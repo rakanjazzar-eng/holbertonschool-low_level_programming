d8679c5562a516cb0be0906854e296c43c92b243
Kony Jazzar <rakanjazzar@gmail.com>
2026-07-16 19:26:42 +0300

    Fix Betty warnings

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * rest_to_98 - Resets a value to 98
+ * reset_to_98 - Resets a value to 98
  * @n: Pointer to the value to reset
  */
 void reset_to_98(int *n)
 {
     *n = 98;
-
-
 }
