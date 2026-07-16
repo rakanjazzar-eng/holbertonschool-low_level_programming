69543ae2966a4ba7c3ab729b5a27f2c85c1060a9
Kony Jazzar <rakanjazzar@gmail.com>
2026-07-16 20:23:12 +0300

    add task 0

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+int _strlen(char *s)
+
+/*
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+    {
+len++;
+    }
+
+	return len;
+}
