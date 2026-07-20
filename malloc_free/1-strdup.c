792fbc0ab082870705c362e0240779d0ffd76446
Kony Jazzar <rakanjazzar@gmail.com>
2026-07-20 17:42:33 +0300

    add task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string, or NULL on failure
+ */
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+int i;
+int size = 0;
+char *dup
+for (i = 0; str[i] != '\0'; i++)
+{
+	size = size + 1;
+}
+
+	dup = malloc(sizeof(char) * (size + 1));
+
+if (dup == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+	dup[i] = str[i];
+}
+dup[size] = '\0';
+
+return (dup);
+}
